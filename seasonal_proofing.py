263230326440bd58eca1ffc8ff8e884659d7983e
malonewalker <mwalker@bestpickreports.com>
2025-06-06 14:57:31 -0400

    Update seasonal_proofing.py

--- seasonal_proofing.py
+++ seasonal_proofing.py
@@ -10,7 +10,7 @@ st.title("Best Pick Reports Seasonal Web Proofing")
 
 uploaded_file = st.file_uploader("Upload the Best Pick Excel file", type=["csv", "xlsx"])
 
-rate_limit = st.slider("Delay between requests (seconds)", min_value=0.0, max_value=5.0, value=1.0, step=0.1)
+
 
 if uploaded_file:
     if uploaded_file.name.endswith(".csv"):
